d4dd699310e1d760f91041d05d54d62c08613493
serkalemnegusse1 <serkalemnegusse1@gmail.com>
2025-02-06 18:35:27 +0300

    Update fraud detection preprocessign for multicollinearity

--- scripts/fraud_detection_preprocessing.py
+++ scripts/fraud_detection_preprocessing.py
@@ -171,5 +171,5 @@ def check_multicollinearity(fraud_df):
     vif_data["feature"] = X.columns
     vif_data["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
     print("\nVariance Inflation Factors (VIF):")
-    print(vif_data)
+    print(vif_data) 
 
